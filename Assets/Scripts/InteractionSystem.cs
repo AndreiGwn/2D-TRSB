60f7934a84df652d0b68e6f26e3e556c786f656e
posadist dolphin <Lucavandoorn@hotmail.com>
2025-02-12 13:46:54 +0100

    finished initial interaction system

---
+++ Assets/Scripts/InteractionSystem.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionSystem : MonoBehaviour
+{
+    public float interactionRange = 3f;
+    public Transform playerTransform;
+    private Interactable target;
+
+
+    private void Start()
+    {
+
+        foreach (Interactable interactable in Interactable.interactables)
+        {
+            Debug.Log(interactable);
+        }
+    }
+
+    private void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+
+        foreach (Interactable interactable in Interactable.interactables)
+        {
+            float d = (playerTransform.position - interactable.gameObject.transform.position).sqrMagnitude;
+            if (d < interactionRange)
+            {
+                target = interactable;
+            }
+        }
+
+        if (target != null && (playerTransform.position - target.gameObject.transform.position).sqrMagnitude > interactionRange)
+        {
+            target = null;
+        }
+    }
+
+    private void Interact()
+    {
+        if (target != null)
+        {
+            Debug.Log(target);
+        }
+    }
+}
